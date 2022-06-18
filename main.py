ced53ed200f6e502b6f9648f706e3482f2e3cd63
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2022-06-18 23:12:32 +0900

    一時休息

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from re import I
 import time
+from psutil import cpu_times
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import settings
@@ -33,8 +35,8 @@ driver.get(URL)
 # button = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/form/div/input")
 # button.send_keys(Keys.ENTER)
 
-# # 遅延処理
-time.sleep(3)
+## 遅延処理
+time.sleep(5)
 
 ## ログイン後画面
 search_box = driver.find_element_by_css_selector(".p-header__search input")
@@ -44,10 +46,41 @@ search_button.send_keys(Keys.ENTER)
 
 ## 検索後画面
 # /* 現在のURLを取得する */
+time.sleep(5)
 current_url = driver.current_url
+
 html = requests.get(current_url)
 soup = BeautifulSoup(html.content, "html.parser")
-print(soup.title)
+
+
+item_list = soup.find(class_="item-list")
+
+## /* アイテムごとの階層構造を取得する */
+item_list_children = [text for text in item_list.children]
+for str in item_list_children:
+    try:
+        ## /* アイテム名と価格を取得する */
+        name = str.find(class_="item-name")
+        price = str.find_all(class_="price")
+
+        ## /* 臨時価格が存在するため、1以上とする */
+        if len(price) >= 1:
+            price_list = [i.text for i in price]
+        else:
+            ## 現在価格が取得できない場合、次の商品情報を処理する
+            continue
+
+        print(name.text)
+        print(price_list)
+
+        ## 遅延処理
+        time.sleep(0.1)
+
+    ## 例外処理
+    except TypeError:
+        continue
+    except AttributeError:
+        continue
 
 time.sleep(10)
 driver.quit()
