a63865009afaae8002e940f7f5ddb6a815a8a5ac
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2022-06-20 22:06:51 +0900

    DB結合

--- app/create_sqlite.py
+++ app/create_sqlite.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import  sqlite3
 
-def create_sqlite(df, alphabet):
+def create_sqlite(df):
     file_sqlite3 = "./items.db"
     conn = sqlite3.connect(file_sqlite3)
     ## 検索したワード（ローマ字）でテーブルを作成する）
-    df.to_sql(f'items_{alphabet}' ,conn,if_exists='append',index=None)
+    df.to_sql('items',conn,if_exists='append',index=None)
     conn.close()
--- app/read_sqlite.py
+++ app/read_sqlite.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import  sqlite3
+import settings
 
-def read_sqlite(alphabet):
+def read_sqlite(search_word):
     file_sqlite3 = "./items.db"
+    DF_COLUMNS = settings.DF_COLUMNS
     conn = sqlite3.connect(file_sqlite3)
-    df = pd.read_sql_query(f'SELECT DISTINCT * FROM items_{alphabet}', conn)
+    df = pd.read_sql_query(f'SELECT DISTINCT * FROM items where "{DF_COLUMNS[0]}" LIKE "%{search_word}%"', conn)
     conn.close()
     return df
