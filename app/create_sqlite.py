6a3aad6e5384838c6e2749cd783f400732b67799
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2022-06-20 22:37:52 +0900

    set

--- app/create_sqlite.py
+++ app/create_sqlite.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import  sqlite3
 
-def create_sqlite(df, alphabet):
+def create_sqlite(df):
     file_sqlite3 = "./db/items.db"
     conn = sqlite3.connect(file_sqlite3)
     ## 検索したワード（ローマ字）でテーブルを作成する）
